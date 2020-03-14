b15c7ca7ff36daa0da1fb5e3fafc008f4017624e
dina <dinafadila@users.noreply.github.com>
2020-03-14 11:30:51 +0700

    Dashboard, sidebar, navbar

---
+++ resources/layout/sidebar.blade.php
@@ -0,0 +1,48 @@
+<aside class="main-sidebar">
+    <!-- sidebar: style can be found in sidebar.less -->
+    <section class="sidebar">
+      <!-- Sidebar user panel -->
+      <div class="user-panel">
+        <div class="pull-left image">
+          <img src="dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
+        </div>
+        <div class="pull-left info">
+          <p>Admin</p>
+          <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
+        </div>
+      </div>
+      <!-- search form -->
+      <form action="#" method="get" class="sidebar-form">
+        <div class="input-group">
+          <input type="text" name="q" class="form-control" placeholder="Search...">
+          <span class="input-group-btn">
+                <button type="submit" name="search" id="search-btn" class="btn btn-flat"><i class="fa fa-search"></i>
+                </button>
+              </span>
+        </div>
+      </form>
+      <!-- /.search form -->
+      <!-- sidebar menu: : style can be found in sidebar.less -->
+      <ul class="sidebar-menu" data-widget="tree">
+        <li class="header">MAIN NAVIGATION</li>
+        <li><a href="https://adminlte.io/docs"><i class="fa fa-dashboard"></i> <span>Dashboard</span></a></li>
+        <li class="active treeview">
+          <a href="#">
+            <i class="fa fa-user"></i> <span>User</span>
+            <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+          </a>
+          <ul class="treeview-menu">
+            <li><a href="index2.html"><i class="fa fa-circle-o"></i> Siswa</a></li>
+            <li><a href="index2.html"><i class="fa fa-circle-o"></i> Pegawai</a></li>
+            <li><a href="index2.html"><i class="fa fa-circle-o"></i> Masyarakat</a></li>
+          </ul>
+        </li>
+        <li><a href="https://adminlte.io/docs"><i class="fa fa-book"></i> <span>Buku</span></a></li>
+        <li><a href="https://adminlte.io/docs"><i class="fa fa-calendar"></i> <span>Dashboard</span></a></li>
+        <li><a href="https://adminlte.io/docs"><i class="fa fa-reply"></i> <span>Peminjaman</span></a></li>
+      </ul>
+    </section>
+    <!-- /.sidebar -->
+  </aside>
