007acb7a2dbde9936292b4cb6f20c516fc6a6f41
dina <dinafadila@users.noreply.github.com>
2020-05-15 20:01:55 +0700

    Pengaturan buku

--- resources/views/Buku/pengaturan.blade.php
+++ resources/views/Buku/pengaturan.blade.php
@@ -2,7 +2,7 @@
 @extends ('navbar')
 
 @section('content')
-<title>Isi title</title>
+<title>Pengaturan Buku</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"> 
     <!-- jQuery library -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
