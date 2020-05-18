484ae2b341ad60a3ba6792a99f55b0bc511bcc33
JekMa <39586664+JekMa@users.noreply.github.com>
2020-05-18 11:44:51 +0700

    Fixing DB

--- database/migrations/2020_05_18_114147_create_santri_table.php
+++ database/migrations/2020_05_18_114147_create_santri_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateSantrisTable extends Migration
+class CreateSantriTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateSantrisTable extends Migration
      */
     public function up()
     {
-        Schema::create('santris', function (Blueprint $table) {
+        Schema::create('santri', function (Blueprint $table) {
             $table->id();
             $table->string('NIST');
             $table->string('Nama'); 
@@ -29,6 +29,6 @@ class CreateSantrisTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('santris');
+        Schema::dropIfExists('santri');
     }
 }
--- database/migrations/2020_05_18_114316_create_kunjugan_siswa_table.php
+++ database/migrations/2020_05_18_114316_create_kunjugan_siswa_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateKunjunganSiswasTable extends Migration
+class CreateKunjuganSiswaTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateKunjunganSiswasTable extends Migration
      */
     public function up()
     {
-        Schema::create('kunjungan_siswa', function (Blueprint $table) {
+        Schema::create('kunjugan_siswa', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
         });
@@ -26,6 +26,6 @@ class CreateKunjunganSiswasTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('kunjungan_siswas');
+        Schema::dropIfExists('kunjugan_siswa');
     }
 }
